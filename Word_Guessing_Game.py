6a861fb2419706e9605ea40caa046fcea7509735
Saad Shaikh <saadshaikh400695@gmail.com>
2025-06-18 20:31:12 +0530

    a game

---
+++ Word_Guessing_Game.py
@@ -0,0 +1,19 @@
+import random 
+
+number = random.randint(1, 100)
+attempts = 0
+
+print("Welcome to the Word Guessing Game..!! Made by @Saad Shaikh")
+print("I have picked a number between 1 to 100.Can you guess it correctly...??")
+
+while True:
+    guess = int(input("Enter your guess: "))
+    attempts += 1
+
+    if guess < number:
+        print("Too Low..!! Try Again.")
+    elif guess > number:
+        print("Too High..!! Try Again.")
+    else:
+        print(f"Congratulations..!! You guessed the number {number} correctly in {attempts} attempts.")
+        break
